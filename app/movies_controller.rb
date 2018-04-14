3a4814a05bd298ead104fbf0060cc09a72940296
Shun Yao <ysmiracle@hotmail.com>
2018-04-14 21:03:00 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,8 +25,8 @@ end
 
 def can_be_created_in_a_block(args = nil)
   Movie.create do |m|
-    m.title = args[0]
-    m.release_date = args[1]
+    m.title = args[0] || "Home Alone"
+    m.release_date ||= args[1] || 1990
   end
 end
 
