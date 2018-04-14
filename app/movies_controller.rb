f396fe33ce5626a15919e5a1a07c1a440ea11c53
Shun Yao <ysmiracle@hotmail.com>
2018-04-14 20:11:11 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,7 +20,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.new(title: attributes[:title], release_date: attributes[:release_date], director: attributes[:director], lead: attributes[:lead], in_theaters: attributes[:in_theaters])
+  movie = Movie.new(attributes)
 end
 
 def can_be_created_in_a_block(args = __)
